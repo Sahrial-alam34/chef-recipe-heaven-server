f2e50adee82e9124203233d6e4266c983ced4e45
Sahrial-alam34 <sunnykcp01@gmail.com>
2023-05-02 12:33:12 +0600

    chef data added and cors

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express  = require('express');
 const app  = express();
+const cors = require('cors');
 
 const port = process.env.PORT || 5000;
-
+app.use(cors());
+const chefDetails = require('./data/chefDetails.json');
 app.get('/', (req, res)=>{
     res.send('Food  is running')
 });
+
+app.get('/chefDetails',(req, res)=>{
+    res.send(chefDetails)
+})
 
 app.listen(port, ()=>{
     console.log(`Food API is running on port: ${port}`)
