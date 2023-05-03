9b058842af14d736f6939ecfcd2b4d1527031d8e
Sahrial-alam34 <sunnykcp01@gmail.com>
2023-05-03 12:11:03 +0600

    food details and cetegory file added

--- index.js
+++ index.js
@@ -5,12 +5,20 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 app.use(cors());
 const chefDetails = require('./data/chefDetails.json');
+const foodCategories = require('./data/foodCategories.json')
+const foodDetails = require('./data/foodDetails.json');
 app.get('/', (req, res)=>{
     res.send('Food  is running')
 });
 
 app.get('/chefDetails',(req, res)=>{
     res.send(chefDetails)
+})
+app.get('/foodCategories',(req, res)=>{
+    res.send(foodCategories)
+})
+app.get('/food',(req,res)=>{
+    res.send(foodDetails);
 })
 
 app.listen(port, ()=>{
